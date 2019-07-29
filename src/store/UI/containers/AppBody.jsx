761107a5c5cd0d94411844b75b4630b8f5f098d1
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-07-29 13:16:57 -0700

    unused import

--- src/store/UI/containers/AppBody.jsx
+++ src/store/UI/containers/AppBody.jsx
@@ -1,6 +1,5 @@
 import { connect } from 'react-redux'
 import {AppBody as AppBodyC} from "../../../app/Styled";
-import {setNavOpen, setSearchOpen, setSecondaryNavOpen} from "../actions";
 
 const mapStateToProps = (state)=>({
 	navOpen: state.UI.navOpen,
