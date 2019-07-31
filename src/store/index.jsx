4063f26ff87abc04d776229af7afc1ccd89364f3
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-07-31 01:42:29 -0700

    back to mainnet

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import UIReducer from "./UI/reducers";
 import Blockchain from "./Blockchain/reducers";
 import sdk from '@kinecosystem/kin-sdk'
-const server = new sdk.Server('https://horizon-ecosystem.kininfrastructure.com');
+const server = new sdk.Server('https://horizon.kinfederation.com/');
 
 
 const rootReducer = combineReducers({
