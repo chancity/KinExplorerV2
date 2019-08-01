2547b290f41308c1bc42f3c2b308bc7b81cf34a7
Chancey Hawbaker <chancey.hawbaker@petco.com>
2019-08-01 00:01:03 -0700

    Removed unfinished links

--- src/layout/Header/NavLinks.jsx
+++ src/layout/Header/NavLinks.jsx
@@ -6,17 +6,17 @@ import {Seperator} from "./Styled/Navbar";
 export const NavLinks = ({onClick}) => {
 	return (
 		<Wrapper>
-			<NavLink to={"/my-kin-wallet"} onClick={onClick} activeClassName={"navActive"}>
+			{/*<NavLink to={"/my-kin-wallet"} onClick={onClick} activeClassName={"navActive"}>
 				My Kin Wallet
-			</NavLink>
+			</NavLink>*/}
 			<Seperator/>
 			<NavLink to={"/apps"} onClick={onClick} activeClassName={"navActive"}>
 				Apps
 			</NavLink>
 			<Seperator/>
-			<NavLink to={"/stats"} onClick={onClick} activeClassName={"navActive"}>
+			{/*<NavLink to={"/stats"} onClick={onClick} activeClassName={"navActive"}>
 				Stats
-			</NavLink>
+			</NavLink>*/}
 		</Wrapper>
 	);
 };
